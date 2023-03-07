499ae18d0ee3ba71622526358fb7e100d1fe9d9a
ildar r. khasanshin <ildar.khasanshin@gmail.com>
2023-03-07 23:07:48 +0500

    [*] issue#2

--- __init__.py
+++ __init__.py
@@ -3,6 +3,19 @@ from cudatext import *
 
 
 class Command:
+    def __init__(self):
+        self.ruler_init = ed.get_prop(PROP_RULER)
+
+    def ruler_set(self, bool_show, text = ''):
+        ed.set_prop(PROP_RULER, bool_show)
+        ed.set_prop(PROP_RULER_TEXT, text)
+
+    def check_ruler_init(self):
+        if self.ruler_init:
+            self.ruler_set(True)
+        else:
+            self.ruler_set(False)
+
     def get_fold_block(self, y):
         for fold in reversed(ed.folding(FOLDING_GET_LIST)):
             fold_0 = fold[0]
@@ -11,14 +24,15 @@ class Command:
                 return fold_0
         return
 
-    def sfl(self):
+    def fr(self):
         res = self.get_fold_block(ed.get_carets()[0][1])
         if res is not None:
-            ed.set_prop(PROP_RULER, True)
-            ed.set_prop(PROP_RULER_TEXT, ed.get_text_line(res).strip())
+            self.ruler_set(True, ed.get_text_line(res).strip())
         else:
-            ed.set_prop(PROP_RULER, False)
-            ed.set_prop(PROP_RULER_TEXT, '')
+            self.check_ruler_init()
 
     def on_caret_slow(self, ed_self):
-        self.sfl()
+        self.fr()
+
+    def on_exit(self, ed_self):
+        self.check_ruler_init()
